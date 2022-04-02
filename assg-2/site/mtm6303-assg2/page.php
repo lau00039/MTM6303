c07d6d3f1a4ab13e84cccb97d4435eb32f3f892d
Christine Lau <lau00039@algonquinlive.com>
2022-04-01 21:23:59 -0400

    push theme codes to show professor

---
+++ assg-2/site/mtm6303-assg2/page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage MTM6303 Assignment
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
